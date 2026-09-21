37bd3572276024581de81dc6d7fa350622dd689d
szkol14-18 <borowieckikamil27@gmail.com>
2026-09-21 22:30:39 +0200

    Stawianie lokalnie postgresa

---
+++ dodatkowe/bazapostgress.py
@@ -0,0 +1,11 @@
+# Jakaś testowa baza
+
+
+# --> https://www.pythonanywhere.com/ --> hostowanie apki przykładowe
+
+# --> https://blog.jsystems.pl/show_post/bezplatny_kurs_administracji_postgresql/
+# Kurs bezpłatny postgresa --> pamiętajcie, że wszystko się co chwilę zmienia, więc
+# czasami coś krok po kroku wedlug instrukcji może troszkę inaczej u was wyglądać.
+
+# --> https://www.youtube.com/watch?v=4qH-7w5LZsA&list=PLJVcjChYKnPcldzUsQBkac5QOjKIyP48a
+# Instalacja swojej własnej bazy do postgresa  lokalnie (do testowania)
