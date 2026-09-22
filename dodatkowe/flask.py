dcc531966957eef6c1e2ec133c96c5fc7b991623
szkol14-18 <borowieckikamil27@gmail.com>
2026-09-22 14:48:57 +0200

    wyjasnienia flask

---
+++ dodatkowe/flask.py
@@ -0,0 +1,16 @@
+# Linki wyjaśniające flaska:
+
+# https://jsystems.pl/blog/show_post/Framework_Flask_-_tworzenie_aplikacji_webowej__i_wy%C5%9Bwietlanie_danych/
+# https://jsystems.pl/blog/show_post/Framework_Flask_-_odczyt_parametr%C3%B3w_z_paska/
+# https://jsystems.pl/blog/show_post/Framework_Flask_-_umieszczanie_i_odczyt_danych_z_sesji/
+# https://jsystems.pl/blog/show_post/Framework_Flask_-_obs%C5%82uga_formularzy/
+# https://jsystems.pl/blog/show_post/Framework_Flask_-_proste_us%C5%82ugi_sieciowe/
+
+# Inna metoda zmian w bazie za pomocą importu modułu SQL_ALchemy + Docker
+# https://jsystems.pl/blog/show_post/Framework_Flask_i_SQL_Alchemy/
+# https://jsystems.pl/blog/show_post/Osadzanie_aplikacji_Flask_na_Dockerze/
+
+# Dodatkowo seria na yt odpowiadająca flaskowi:
+# https://www.youtube.com/watch?v=mqhxxeeTbu0&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX
+
+
